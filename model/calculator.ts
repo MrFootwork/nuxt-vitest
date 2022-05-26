cb83ff44fab4442660cad61618102cf907b873f6
Mr Footwork <pandau.ting@gmail.com>
2022-05-26 22:54:07 +0200

    completed calculator ✔

--- model/calculator.ts
+++ model/calculator.ts
@@ -5,5 +5,11 @@ const calculator = {
 	subtract: function (num1, num2) {
 		return num1 - num2
 	},
+	multiply: function (num1, num2) {
+		return num1 * num2
+	},
+	divide: function (num1, num2) {
+		return num1 / num2
+	},
 }
 export default calculator
--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -10,4 +10,14 @@ describe('calculator', () => {
 		expect(calculator.subtract(10, 2)).toBe(8)
 		expect(calculator.subtract(0, 0)).toBe(0)
 	})
+	it('multiplies', () => {
+		expect(calculator.multiply(10, 2)).toBe(20)
+		expect(calculator.multiply(0, 0)).toBe(0)
+		expect(calculator.multiply(12, 1)).toBe(12)
+	})
+	it('divides', () => {
+		expect(calculator.divide(10, 2)).toBe(5)
+		expect(calculator.divide(0, 0)).toBe(NaN)
+		expect(calculator.divide(12, 1)).toBe(12)
+	})
 })
